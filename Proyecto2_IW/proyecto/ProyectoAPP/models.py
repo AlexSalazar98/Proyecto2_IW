530e0021300e5a95b2150465b5cbd030ecd6a289
AlexSalazar98 <alex.salazar@opendeusto.es>
2020-04-02 21:20:36 +0200

    Acabado por hoy

--- Proyecto2_IW/proyecto/ProyectoAPP/models.py
+++ Proyecto2_IW/proyecto/ProyectoAPP/models.py
@@ -6,19 +6,19 @@ class Clientes(models.Model):
     nombre = models.CharField(max_length=30)
     empresa = models.CharField(max_length=50)
     email = models.EmailField()
-    localizacion = models.CharField()
-    telefono = models.IntegerField(max_length=9)
-    numero_cuenta = models.CharField()
+    localizacion = models.TextField()
+    telefono = models.IntegerField()
+    numero_cuenta = models.CharField(max_length=24)
 
 
 # Clase para categoria usuario
 class Categoria(models.Model):
-    nombre = models.CharField()
+    nombre = models.CharField(max_length=30)
 
 
 # Clase para departamentos
 class Departamento(models.Model):
-    nombre = models.CharField()
+    nombre = models.CharField(max_length=30)
 
 
 # Clase para la creacion de Usuarios
@@ -36,8 +36,11 @@ class Empleados(models.Model):
     nombre = models.CharField(max_length=10)
     apellido = models.CharField(max_length=10)
     email = models.EmailField()
-    telefono = models.IntegerField(max_length=9)
+    telefono = models.IntegerField()
 
+#Clase estado para recopilar el estado de las tareas
+class Estado(models.Model):
+    estado = models.CharField(max_length=20, default="Sin asignar")
 
 # Clase para la creacion de Tareas
 class Tareas(models.Model):
@@ -45,9 +48,9 @@ class Tareas(models.Model):
     descripcion = models.TextField()
     fecha_inicio = models.DateField()
     fecha_fin = models.DateField()
-    responsable = models.ForeignKey(Empleados, on_delete=models.SET_NULL)
-    nivel_prioridad = models.CharField()
-    estado_tarea = models.CharField()
+    responsable = models.ForeignKey(Empleados, on_delete=models.CASCADE)
+    nivel_prioridad = models.CharField(max_length=20)
+    estado_tarea = models.ForeignKey(Estado, on_delete=models.CASCADE)
     notas_adicionales_escritas_empleado = models.TextField()
 
 
@@ -60,5 +63,5 @@ class Proyectos(models.Model):
     presupuesto = models.IntegerField()
     cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
     tareas_a_realizar = models.ManyToManyField(Tareas)
-    empleados = models.ManyToManyField(Empleados, on_delete=models.CASCADE)
+    empleados = models.ManyToManyField(Empleados)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
