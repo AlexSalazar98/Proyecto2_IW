cadbf752e5e20278d7f5bfbcf9c19ca3a3913dd0
AlexSalazar98 <alex.salazar@opendeusto.es>
2020-04-06 21:02:37 +0200

    pop-up hecho

---
+++ Proyecto2_IW/ParteVisual/HTML/popup.js
@@ -0,0 +1,16 @@
+var enlAbrirPopup = document.getElementById('enlAbrirPopup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('pop-up'),
+	enlCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+
+enlAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+	
+enlCerrarPopup.addEventListener('click', function(){
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+		
--- Proyecto2_IW/proyecto/ProyectoAPP/models.py
+++ Proyecto2_IW/proyecto/ProyectoAPP/models.py
@@ -11,7 +11,7 @@ class Clientes(models.Model):
     numero_cuenta = models.CharField(max_length=24)
 
 
-# Clase para categoria usuario
+# Clase para categoria usuario (Jefe dep. / Gerente / Ingeniero
 class Categoria(models.Model):
     nombre = models.CharField(max_length=30)
 
@@ -28,6 +28,7 @@ class Usuarios(models.Model):
     email = models.EmailField()
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    contraseña = models.CharField(max_length=10)
 
 
 # Clase para la creacion de Empleados
@@ -38,12 +39,14 @@ class Empleados(models.Model):
     email = models.EmailField()
     telefono = models.IntegerField()
 
-#Clase estado para recopilar el estado de las tareas
+
+# Clase estado para recopilar el estado de las tareas
 class Estado(models.Model):
     estado = models.CharField(max_length=20, default="Abierta")
 
     def __str__(self):
         return f"Estado = {self.estado}"
+
 
 # Clase para la creacion de Tareas
 class Tareas(models.Model):
