4c38ad27801ddfc78a2a544f4989edc7d1e35119
AlexSalazar98 <alex.salazar@opendeusto.es>
2020-04-11 13:04:50 +0200

    desarollado formulario de registro

---
+++ Proyecto2_IW/proyecto/ProyectoAPP/static/js/comprobar_cont_form.js
@@ -0,0 +1,6 @@
+var cont1 = document.getElementById(''),
+	cont2 = document.getElementById('');
+
+
+if (cont1 != cont2) 
+		alert("Las dos claves no coinciden") 
--- Proyecto2_IW/proyecto/ProyectoAPP/views.py
+++ Proyecto2_IW/proyecto/ProyectoAPP/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from ProyectoAPP.models import Usuarios, Departamento, Categoria
+
+
 # import js2py
 
 
@@ -47,13 +49,7 @@ def login(request):
                 # return HttpResponse(f"usuario {usuario} contraseña: {contraseña}; {usuario_registrado} ")
                 return render(request, 'TablaPrincipal.html')
                 break
-            #else:
-                #js2py.run_file('ProyectoAPP\static\js\popup.js')
-                # exec(open('popup.js').read())
-                #context = {
-                   # 'contrasena_incorrecta': 'Usuario o contraseña incorrectos'
-                #}
-                # return render(request, 'index.html', context)
+
 
 def PaginaPricipal(request):
     return render(request, 'TablaPrincipal.html')
@@ -62,8 +58,11 @@ def PaginaPricipal(request):
 def LlamarFormulario(request):
     departamentos = Departamento.objects.order_by('nombre')
     categoria = Categoria.objects.order_by('nombre')
-
-    return render(request, 'Formulario.html', departamentos, categoria)
+    context ={
+        'departamentos': departamentos,
+        'categoria': categoria
+    }
+    return render(request, 'Formulario.html', context)
 
 
 def RecogerFormulario(request):
@@ -71,7 +70,7 @@ def RecogerFormulario(request):
     nombre = request.POST["nombre"]
     apellido1 = request.POST["Apellido_1"]
     apellido2 = request.POST["Apellido_2"]
-    #Sexo
+    Sexo = request.POST.get('Sexo')
     fecha_nacimiento = request.POST["Fecha_Nacimiento"]
     departamento = request.POST["Departamento"]
     categoria = request.POST["Categoria"]
@@ -80,7 +79,8 @@ def RecogerFormulario(request):
     repContraseña = request.POST["Repetir_Contraseña"]
     email = request.POST["Correo_Electronico"]
 
-    return HttpResponse(f"nombre {nombre} apellido 1 {apellido1} apellido 2 {apellido2} fecha nacimiento {fecha_nacimiento}"
-                        f" departamentos {departamento} categoria {categoria} usuario {usuario} contraseña {contraseña}"
-                        f" repContraseña {repContraseña} email {email} ")
-    #return render(request, 'Formulario.html')
+    return HttpResponse(
+        f"nombre {nombre} apellido 1 {apellido1} apellido 2 {apellido2} sexo {Sexo} fecha nacimiento {fecha_nacimiento}"
+        f" departamentos {departamento} categoria {categoria} usuario {usuario} contraseña {contraseña}"
+        f" repContraseña {repContraseña} email {email} ")
+    # return render(request, 'Formulario.html')
