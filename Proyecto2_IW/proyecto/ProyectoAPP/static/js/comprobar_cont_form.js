609049c22ba2f883c95cf52685e365c42a4495b4
AlexSalazar98 <alex.salazar@opendeusto.es>
2020-04-11 14:02:38 +0200

    js comprobar contraseñas

--- Proyecto2_IW/proyecto/ProyectoAPP/static/js/comprobar_cont_form.js
+++ Proyecto2_IW/proyecto/ProyectoAPP/static/js/comprobar_cont_form.js
@@ -1,6 +1,8 @@
-var cont1 = document.getElementById(''),
-	cont2 = document.getElementById('');
+var cont1 = document.getElementById('Contraseña').value,
+	cont2 = document.getElementById('Repetir_Contraseña').value;
 
 
-if (cont1 != cont2) 
-		alert("Las dos claves no coinciden") 
+if (cont1 != cont2) {
+		    alert("Las dos claves no coinciden");
+		    return false;
+		}
