83724ec05e5473cf33d6a3c921f53c2ff4bdcfa0
Unknown <david.requeta@opendeusto.es>
2020-04-14 12:02:16 +0200

    contraseñas ok

--- Proyecto2_IW/proyecto/ProyectoAPP/static/js/comprobar_cont_form.js
+++ Proyecto2_IW/proyecto/ProyectoAPP/static/js/comprobar_cont_form.js
@@ -1,7 +1,7 @@
 function comprobarClave() {
 
-    var cont1 = document.getElementById('Contraseña'),
-	    cont2 = document.getElementById('Repetir_Contraseña');
+    var cont1 = document.getElementById('Contraseña').value;
+	    cont2 = document.getElementById('Repetir_Contraseña').value;
 
     if (cont1 != cont2)  {
         alert('Las contraseñas no coinciden');
