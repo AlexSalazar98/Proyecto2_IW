ed848e2ec0c59b101ef931bc39b5717402130446
AlexSalazar98 <alex.salazar@opendeusto.es>
2020-04-14 17:46:09 +0200

    seguimos con las contrseñas

--- Proyecto2_IW/proyecto/ProyectoAPP/static/js/comprobar_cont_form.js
+++ Proyecto2_IW/proyecto/ProyectoAPP/static/js/comprobar_cont_form.js
@@ -1,11 +1,12 @@
 function comprobarClave() {
 
-    var cont1 = document.getElementById('Contraseña'),
-	    cont2 = document.getElementById('Repetir_Contraseña');
+    var cont1 = document.getElementById('Contraseña').value,
+	    cont2 = document.getElementById('Repetir_Contraseña').value;
 
     if (cont1 != cont2)  {
         alert('Las contraseñas no coinciden');
     }else{
-        document.Form_regist.submit();
+        document.getElementById('Alta_Registro').submit();
+        return " url 'RecogerFormulario' "
     }
 }
